b4747f11b5b4a43197b97f9d843fdd3102f7ad1a
Abhinav Sharma <abhinav.ih@gmail.com>
2016-11-26 23:53:34 +0530

    base activity added

---
+++ app/src/main/java/com/android/shopr/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.android.shopr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abhinav.sharma on 11/26/2016.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
