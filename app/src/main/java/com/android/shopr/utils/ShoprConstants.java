0b02ec9dccb5c1a1e3750a7d9919f78a3304f9a5
Abhinav Sharma <abhinav.ih@gmail.com>
2016-12-25 13:00:24 +0530

    Shopr constants added

---
+++ app/src/main/java/com/android/shopr/utils/ShoprConstants.java
@@ -0,0 +1,11 @@
+package com.android.shopr.utils;
+
+/**
+ * Created by abhinav.sharma on 12/25/2016.
+ */
+
+public class ShoprConstants {
+
+    public static final String APP_PREFERENCE = "shopr_preference";
+    public static final String USER_PROFILE_PREFS_KEYS = "user_profile";
+}
