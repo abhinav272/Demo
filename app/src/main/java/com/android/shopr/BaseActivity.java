dd1b25b92d749d33db4d16de0c62fef4368ea1cb
Abhinav Sharma <abhinav.ih@gmail.com>
2016-12-30 19:48:18 +0530

    Scanner functionality integrated to base activity to be scalable

--- app/src/main/java/com/android/shopr/BaseActivity.java
+++ app/src/main/java/com/android/shopr/BaseActivity.java
@@ -2,9 +2,14 @@ package com.android.shopr;
 
 import android.support.v7.app.AppCompatActivity;
 
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
 /**
  * Created by abhinav.sharma on 11/26/2016.
  */
 
 public class BaseActivity extends AppCompatActivity {
+    public void showScannerView(ZXingScannerView mScannerView) {
+        setContentView(mScannerView);
+    }
 }
