3d918d62b5c2930a541ccf29a37e0f3f5f1b5912
Abhinav Sharma <abhinav.ih@gmail.com>
2017-01-10 22:20:46 +0530

    shopr constants updated

--- app/src/main/java/com/android/shopr/utils/ShoprConstants.java
+++ app/src/main/java/com/android/shopr/utils/ShoprConstants.java
@@ -8,4 +8,5 @@ public class ShoprConstants {
 
     public static final String APP_PREFERENCE = "shopr_preference";
     public static final String USER_PROFILE_PREFS_KEYS = "user_profile";
+    public static final String BASE_URL = "http://34.196.71.31:8080/v1/rest/auth";
 }
