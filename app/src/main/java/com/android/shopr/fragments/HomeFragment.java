ed9d3d4f66dd3e32b5fedc3ad0ce15cef535750c
Abhinav Sharma <abhinav.ih@gmail.com>
2017-01-20 23:29:58 +0530

    Home fragment updated with stores api call

--- app/src/main/java/com/android/shopr/fragments/HomeFragment.java
+++ app/src/main/java/com/android/shopr/fragments/HomeFragment.java
@@ -2,18 +2,33 @@ package com.android.shopr.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.shopr.R;
+import com.android.shopr.adapters.TwoImageRecyclerViewAdapter;
+import com.android.shopr.api.ShoprAPIClient;
+import com.android.shopr.model.Store;
+import com.android.shopr.utils.ExecutorSupplier;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by abhinav.sharma on 1/8/2017.
  */
 
-public class HomeFragment extends BaseFragment {
+public class HomeFragment extends BaseFragment implements Callback<Store.List> {
 
+    private static final String TAG = "HomeFragment";
+    private RecyclerView mRecyclerView;
+    private TwoImageRecyclerViewAdapter mTwoImageRecyclerViewAdapter;
+    private Store.List mStores;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,5 +38,37 @@ public class HomeFragment extends BaseFragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(getLayoutManager());
+        getAllStores();
+    }
+
+    private void getAllStores() {
+        ExecutorSupplier.getInstance().getWorkerThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                Call<Store.List> call = ShoprAPIClient.getApiInterface().getAllStores();
+                call.enqueue(HomeFragment.this);
+            }
+        });
+    }
+
+    private GridLayoutManager getLayoutManager(){
+        return new GridLayoutManager(getActivity(), 2);
+    }
+
+    @Override
+    public void onResponse(Call<Store.List> call, Response<Store.List> response) {
+        mStores = response.body();
+        mTwoImageRecyclerViewAdapter = new TwoImageRecyclerViewAdapter(mStores, getActivity());
+        mRecyclerView.setAdapter(mTwoImageRecyclerViewAdapter);
+        for (Store s:mStores) {
+            Log.e("onResponse: ", s.getStoreName());
+        }
+    }
+
+    @Override
+    public void onFailure(Call<Store.List> call, Throwable t) {
+        Log.e(TAG, "onFailure: ", t);
     }
 }
