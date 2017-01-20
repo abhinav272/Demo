504df94f326767fbe32330ec690011a0209f131d
Abhinav Sharma <abhinav.ih@gmail.com>
2017-01-20 23:31:58 +0530

    revoke access method added to utils

--- app/src/main/java/com/android/shopr/utils/Utils.java
+++ app/src/main/java/com/android/shopr/utils/Utils.java
@@ -1,8 +1,30 @@
 package com.android.shopr.utils;
 
+import android.content.Context;
+import android.util.Log;
+
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by abhinav.sharma on 18/01/17.
  */
 
 public class Utils {
+
+    private static final String TAG = "Utils";
+
+    public static void clearAllDataAndRevokeAccess(final Context context) {
+        ExecutorSupplier.getInstance().getWorkerThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+                PreferenceUtils.getInstance(context).saveUserProfile(null);
+            }
+        });
+        Log.d(TAG, "revokeAccess: ");
+        FacebookSdk.sdkInitialize(context.getApplicationContext());
+        LoginManager.getInstance().logOut();
+        FirebaseAuth.getInstance().signOut();
+    }
 }
