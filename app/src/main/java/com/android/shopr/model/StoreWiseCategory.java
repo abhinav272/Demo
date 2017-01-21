c5a8ea6645dce5e8f9659356a51dd8a9f7ab28b2
Abhinav Sharma <abhinav.ih@gmail.com>
2017-01-21 15:02:51 +0530

    pojo for categories specific to store

---
+++ app/src/main/java/com/android/shopr/model/StoreWiseCategory.java
@@ -0,0 +1,36 @@
+package com.android.shopr.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+/**
+ * Created by abhinav.sharma on 21/01/17.
+ */
+
+public class StoreWiseCategory {
+
+    @SerializedName("store_id")
+    @Expose
+    private int storeId;
+    @SerializedName("categories")
+    @Expose
+    private List<Category> categories = null;
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+}
