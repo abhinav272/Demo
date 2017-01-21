ded644e02981f736b3651e267dbfa3c8bf16f4c4
Abhinav Sharma <abhinav.ih@gmail.com>
2017-01-21 15:00:38 +0530

    file name refactored

--- app/src/main/java/com/android/shopr/adapters/StoresRecyclerViewAdapter.java
+++ app/src/main/java/com/android/shopr/adapters/StoresRecyclerViewAdapter.java
@@ -15,14 +15,20 @@ import com.squareup.picasso.Picasso;
  * Created by abhinav.sharma on 20/01/17.
  */
 
-public class TwoImageRecyclerViewAdapter extends RecyclerView.Adapter<SingleImageAndTextViewHolder> {
+public class StoresRecyclerViewAdapter extends RecyclerView.Adapter<SingleImageAndTextViewHolder>{
+
+    public interface DelegateEvent{
+        void delegateToHost(int position);
+    }
 
     private Store.List mStores;
     private Context mContext;
+    private DelegateEvent delegateEvent;
 
-    public TwoImageRecyclerViewAdapter(Store.List mStores, Context mContext) {
+    public StoresRecyclerViewAdapter(Store.List mStores, Context mContext, DelegateEvent delegateEvent) {
         this.mStores = mStores;
         this.mContext = mContext;
+        this.delegateEvent = delegateEvent;
     }
 
     @Override
@@ -32,9 +38,15 @@ public class TwoImageRecyclerViewAdapter extends RecyclerView.Adapter<SingleImag
     }
 
     @Override
-    public void onBindViewHolder(SingleImageAndTextViewHolder holder, int position) {
+    public void onBindViewHolder(SingleImageAndTextViewHolder holder, final int position) {
         Picasso.with(mContext).load(getItem(position).getImgUrl()).into(holder.mImageView);
         holder.mTextView.setText(getItem(position).getStoreName());
+        holder.mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delegateEvent.delegateToHost(position);
+            }
+        });
     }
 
     @Override
