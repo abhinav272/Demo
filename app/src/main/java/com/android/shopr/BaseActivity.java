41e09abcf5817f7a0385d3a6c15e5c50112e5911
Abhinav Sharma <abhinav.ih@gmail.com>
2017-03-09 22:49:07 +0530

    toast method added for base activity

--- app/src/main/java/com/android/shopr/BaseActivity.java
+++ app/src/main/java/com/android/shopr/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.android.shopr;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -10,4 +11,8 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class BaseActivity extends AppCompatActivity {
 
+
+    public void showShortToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
 }
