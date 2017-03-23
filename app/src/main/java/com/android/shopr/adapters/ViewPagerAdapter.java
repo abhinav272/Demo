b4a2fc17661dd28a6958b0e82933efd932ad393e
Abhinav Sharma <abhinav.ih@gmail.com>
2017-03-23 23:34:09 +0530

    ViewPagerAdapter made functional

--- app/src/main/java/com/android/shopr/adapters/ViewPagerAdapter.java
+++ app/src/main/java/com/android/shopr/adapters/ViewPagerAdapter.java
@@ -1,8 +1,16 @@
 package com.android.shopr.adapters;
 
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+
+import com.android.shopr.fragments.HomeFragment;
+import com.android.shopr.model.PlaceWiseCategories;
+import com.android.shopr.model.PlaceWiseCategoriesStores;
+import com.android.shopr.model.PlaceWiseStores;
+import com.android.shopr.utils.ShoprConstants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,36 +20,45 @@ import java.util.List;
  */
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
-    private List<Fragment> mFragmentList;
-    private List<String> mFragmentTitleList;
+    private PlaceWiseCategoriesStores mPlaceWiseCategoriesStores;
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    public ViewPagerAdapter(FragmentManager fm, PlaceWiseCategoriesStores mPlaceWiseCategoriesStores) {
         super(fm);
-        mFragmentList = new ArrayList<>();
-        mFragmentTitleList = new ArrayList<>();
-//        this.mFragmentList = mFragmentList;
-//        this.mFragmentTitleList = mFragmentTitleList;
-//        if (mFragmentList.size() != mFragmentTitleList.size())
-//            throw new IllegalStateException("Fragment titles and Fragments size mismatch");
+        this.mPlaceWiseCategoriesStores = mPlaceWiseCategoriesStores;
+        List<PlaceWiseCategories> placeWiseCategories = mPlaceWiseCategoriesStores.getCategories();
+        placeWiseCategories.add(0,null);
+        mPlaceWiseCategoriesStores.setCategories(placeWiseCategories);
     }
 
     @Override
     public Fragment getItem(int position) {
-        return mFragmentList.get(position);
+        HomeFragment homeFragment = new HomeFragment();
+        Bundle bundle = new Bundle();
+        ArrayList<PlaceWiseStores> stores = new ArrayList<>();
+        if (position==0){
+            bundle.putParcelableArrayList(ShoprConstants.PLACE_WISE_STORE_LIST, new ArrayList<Parcelable>(mPlaceWiseCategoriesStores.getStores()));
+            homeFragment.setArguments(bundle);
+            return homeFragment;
+        }
+        List<Integer> storeIds = mPlaceWiseCategoriesStores.getCategories().get(position).getStoresList();
+        for (PlaceWiseStores pStores : mPlaceWiseCategoriesStores.getStores()) {
+            if (storeIds.contains(pStores.getStoreId()))
+                stores.add(pStores);
+        }
+        bundle.putParcelableArrayList(ShoprConstants.PLACE_WISE_STORE_LIST, stores);
+        homeFragment.setArguments(bundle);
+        return homeFragment;
     }
 
     @Override
     public int getCount() {
-        return mFragmentList.size();
-    }
-
-    public void addFragment(Fragment fragment, String title) {
-        mFragmentList.add(fragment);
-        mFragmentTitleList.add(title);
+        return mPlaceWiseCategoriesStores.getCategories().size();
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
-        return mFragmentTitleList.get(position);
+        if (position==0)
+            return "All";
+        return mPlaceWiseCategoriesStores.getCategories().get(position).getCategoryName();
     }
 }
