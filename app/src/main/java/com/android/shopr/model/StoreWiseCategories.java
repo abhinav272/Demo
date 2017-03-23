4542fae81b38a97c183ca3fc01669d0ee676726e
Abhinav Sharma <abhinav.ih@gmail.com>
2017-03-23 23:31:57 +0530

    StoreWiseCategories added

---
+++ app/src/main/java/com/android/shopr/model/StoreWiseCategories.java
@@ -0,0 +1,80 @@
+package com.android.shopr.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Abhinav on 23/03/17.
+ */
+public class StoreWiseCategories implements Parcelable {
+    @SerializedName("category_id")
+    @Expose
+    private int categoryId;
+    @SerializedName("category_name")
+    @Expose
+    private String categoryName;
+    @SerializedName("product")
+    @Expose
+    private Product product;
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.categoryId);
+        dest.writeString(this.categoryName);
+        dest.writeParcelable(this.product, flags);
+    }
+
+    public StoreWiseCategories() {
+    }
+
+    protected StoreWiseCategories(Parcel in) {
+        this.categoryId = in.readInt();
+        this.categoryName = in.readString();
+        this.product = in.readParcelable(Product.class.getClassLoader());
+    }
+
+    public static final Creator<StoreWiseCategories> CREATOR = new Creator<StoreWiseCategories>() {
+        @Override
+        public StoreWiseCategories createFromParcel(Parcel source) {
+            return new StoreWiseCategories(source);
+        }
+
+        @Override
+        public StoreWiseCategories[] newArray(int size) {
+            return new StoreWiseCategories[size];
+        }
+    };
+}
