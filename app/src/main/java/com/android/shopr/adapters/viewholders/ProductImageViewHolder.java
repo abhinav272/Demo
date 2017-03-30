4e1e66d0a9cf59554da2eff6d26803d2116883aa
Abhinav Sharma <abhinav.ih@gmail.com>
2017-03-30 23:25:26 +0530

    new ViewHolder created for Products

---
+++ app/src/main/java/com/android/shopr/adapters/viewholders/ProductImageViewHolder.java
@@ -0,0 +1,31 @@
+package com.android.shopr.adapters.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.shopr.R;
+
+/**
+ * Created by Abhinav on 30/03/17.
+ */
+public class ProductImageViewHolder extends RecyclerView.ViewHolder {
+    private static final String TAG = "ProductImageViewHolder";
+    public ImageView mImageView;
+    public TextView productName, originalPrice, priceAfterDiscount, productDiscount;
+
+    public ProductImageViewHolder(View view) {
+        super(view);
+        mImageView = (ImageView) view.findViewById(R.id.iv_image);
+        productName = (TextView) view.findViewById(R.id.tv_product_name);
+        originalPrice = (TextView) view.findViewById(R.id.tv_product_price_original);
+        priceAfterDiscount = (TextView) view.findViewById(R.id.tv_product_price_after_discount);
+        productDiscount = (TextView) view.findViewById(R.id.tv_product_discount);
+    }
+
+    public void onClick(View view) {
+        Log.d(TAG, "onClick: ");
+    }
+}
