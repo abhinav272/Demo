56485f45e7fac1b11425d5094aa5a2953c89cbf2
Abhinav Sharma <abhinav.ih@gmail.com>
2017-03-31 18:44:15 +0530

    Pojo implemets Parcelable

--- app/src/main/java/com/android/shopr/model/CategoryWiseProducts.java
+++ app/src/main/java/com/android/shopr/model/CategoryWiseProducts.java
@@ -1,5 +1,8 @@
 package com.android.shopr.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -9,7 +12,7 @@ import java.util.List;
  * Created by abhinav.sharma on 21/01/17.
  */
 
-public class CategoryWiseProducts {
+public class CategoryWiseProducts implements Parcelable {
     @SerializedName("store_id")
     @Expose
     private int storeId;
@@ -54,4 +57,40 @@ public class CategoryWiseProducts {
     public void setProducts(List<Product> products) {
         this.products = products;
     }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.storeId);
+        dest.writeInt(this.categoryId);
+        dest.writeTypedList(this.products);
+        dest.writeString(this.categoryName);
+    }
+
+    public CategoryWiseProducts() {
+    }
+
+    protected CategoryWiseProducts(Parcel in) {
+        this.storeId = in.readInt();
+        this.categoryId = in.readInt();
+        this.products = in.createTypedArrayList(Product.CREATOR);
+        this.categoryName = in.readString();
+    }
+
+    public static final Creator<CategoryWiseProducts> CREATOR = new Creator<CategoryWiseProducts>() {
+        @Override
+        public CategoryWiseProducts createFromParcel(Parcel source) {
+            return new CategoryWiseProducts(source);
+        }
+
+        @Override
+        public CategoryWiseProducts[] newArray(int size) {
+            return new CategoryWiseProducts[size];
+        }
+    };
 }
