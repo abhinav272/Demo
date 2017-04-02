2e5726e0810f2da268c8afdef35dbb179d4918f0
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-02 23:13:01 +0530

    Cart item view holder created

---
+++ app/src/main/java/com/android/shopr/adapters/viewholders/CartItemViewHolder.java
@@ -0,0 +1,36 @@
+package com.android.shopr.adapters.viewholders;
+
+import android.support.v7.widget.AppCompatSpinner;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.shopr.R;
+
+/**
+ * Created by Abhinav on 02/04/17.
+ */
+public class CartItemViewHolder extends RecyclerView.ViewHolder {
+
+    public ImageView ivProductImage, decreseQty, increaseQty;
+    public TextView productName, productPriceBeforeDiscount, productPriceAfterDiscount, productDiscount, productQuantity, removeProduct, addToWatchList;
+    public AppCompatSpinner spinnerSize;
+
+    public CartItemViewHolder(View itemView) {
+        super(itemView);
+        ivProductImage = (ImageView) itemView.findViewById(R.id.iv_product_image);
+        decreseQty = (ImageView) itemView.findViewById(R.id.iv_decrease_qty);
+        increaseQty = (ImageView) itemView.findViewById(R.id.iv_increase_qty);
+        productName = (TextView) itemView.findViewById(R.id.tv_product_name);
+        productPriceAfterDiscount = (TextView) itemView.findViewById(R.id.tv_product_price_after_discount);
+        productPriceBeforeDiscount = (TextView) itemView.findViewById(R.id.tv_product_price_original);
+        productDiscount = (TextView) itemView.findViewById(R.id.tv_product_discount);
+        productQuantity = (TextView) itemView.findViewById(R.id.tv_product_quantity);
+        removeProduct = (TextView) itemView.findViewById(R.id.tv_remove_product);
+        addToWatchList = (TextView) itemView.findViewById(R.id.tv_move_to_watchlist);
+        spinnerSize = (AppCompatSpinner) itemView.findViewById(R.id.spinner_sizes);
+    }
+
+
+}
