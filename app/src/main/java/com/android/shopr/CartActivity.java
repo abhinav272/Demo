a25ee08be26bf42aacd1ffdf971252b5bf3d6d2b
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-07 19:44:47 +0530

    Saving cart items

--- app/src/main/java/com/android/shopr/CartActivity.java
+++ app/src/main/java/com/android/shopr/CartActivity.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 
 import com.android.shopr.fragments.CartFragment;
+import com.android.shopr.model.Cart;
+import com.android.shopr.model.CartItem;
+import com.android.shopr.utils.PreferenceUtils;
 
 /**
  * Created by Abhinav on 01/04/17.
@@ -12,6 +15,7 @@ import com.android.shopr.fragments.CartFragment;
 public class CartActivity extends BaseActivity {
 
     private FragmentManager mFragmentManager;
+    private Cart cart;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,11 +27,20 @@ public class CartActivity extends BaseActivity {
 
     private void initSetup() {
         mFragmentManager = getSupportFragmentManager();
+        cart = PreferenceUtils.getInstance(this).getUserCart();
     }
 
     private void initCartFragment() {
         mFragmentManager.beginTransaction()
                 .add(R.id.frame_container, new CartFragment(), CartFragment.class.getSimpleName())
                 .commit();
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
     }
 }
