6bde4239f98447642edf7c22a1f2d4b7d4cead31
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-07 19:42:32 +0530

    Options view holder created for cart

---
+++ app/src/main/java/com/android/shopr/adapters/viewholders/OptionsItemViewHolder.java
@@ -0,0 +1,29 @@
+package com.android.shopr.adapters.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.android.shopr.R;
+
+/**
+ * Created by Abhinav on 03/04/17.
+ */
+public class OptionsItemViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView tvBagTotalBeforeDiscount, tvBagDiscount, tvBagTotalAfterDiscount, tvApplyDiscount, tvAmountPayable;
+    public FrameLayout flApplyCoupon, flApplyLoyaltyCard;
+
+    public OptionsItemViewHolder(View itemView) {
+        super(itemView);
+
+        tvBagTotalBeforeDiscount = (TextView) itemView.findViewById(R.id.tv_bag_total);
+        tvBagDiscount = ((TextView) itemView.findViewById(R.id.tv_bag_discount));
+        tvBagTotalAfterDiscount = ((TextView) itemView.findViewById(R.id.tv_total_after_discount));
+        tvApplyDiscount = (TextView) itemView.findViewById(R.id.tv_coupon);
+        tvAmountPayable = (TextView) itemView.findViewById(R.id.tv_total_after_coupon);
+        flApplyCoupon = (FrameLayout) itemView.findViewById(R.id.fl_apply_coupon);
+        flApplyLoyaltyCard = (FrameLayout) itemView.findViewById(R.id.fl_apply_loyalty_card);
+    }
+}
