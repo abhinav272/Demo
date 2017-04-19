cb96e982f028889d163e919bd8001ce0b729a5e4
Abhinav Sharma <abhinav.ih@gmail.com>
2017-04-19 21:29:19 +0530

    Retrofit logs enabled

--- app/src/main/java/com/android/shopr/api/ShoprAPIClient.java
+++ app/src/main/java/com/android/shopr/api/ShoprAPIClient.java
@@ -3,6 +3,8 @@ package com.android.shopr.api;
 import com.android.shopr.utils.NullOnEmptyConverterFactory;
 import com.android.shopr.utils.ShoprConstants;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -21,11 +23,19 @@ public class ShoprAPIClient {
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
                     .baseUrl(ShoprConstants.BASE_URL)
+                    .client(getOkHttpClient())
                     .addConverterFactory(new NullOnEmptyConverterFactory())
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
         return retrofit;
+    }
+
+    private static OkHttpClient getOkHttpClient() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+        return client;
     }
 
     public static APIInterface getApiInterface() {
